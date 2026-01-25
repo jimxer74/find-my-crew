2e2d63210d6c3aefe0df51c926e072b0c5455eae
Jani <jmixer74@yahoo.com>
2026-01-25 16:19:06 +0200

    LogoWithText

--- app/components/LogoWithText.tsx
+++ app/components/LogoWithText.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import Image from 'next/image';
 
 type LogoWithTextProps = {
   className?: string;
@@ -11,17 +12,14 @@ export function LogoWithText({
 }: LogoWithTextProps) {
   return (
     <Link href="/" className={className}>
-      <div 
-        className="px-4 py-2 text-white uppercase font-bold"
-        style={{ 
-          backgroundColor: '#22276E',
-          fontFamily: 'Cascadia Code, monospace',
-          fontWeight: 600,
-          borderRadius: '50% 0% 50% 50% / 120%'
-        }}
-      >
-        CREW.
-      </div>
+      <Image 
+        src="/sailsmart.png" 
+        alt="SailSmart" 
+        width={50} 
+        height={50}
+        priority
+        className="object-contain rounded-lg"
+      />
     </Link>
   );
 }
